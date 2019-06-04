4e0235ecca62b1c1ebfcef95813233461ad19a17
jeterarellano <jeterarellano@10.20.10.210>
2019-06-03 17:21:23 -0700

    Skeleton

--- src/RubiksCube.java
+++ src/RubiksCube.java
@@ -1,4 +1,70 @@
 
 public class RubiksCube {
+	
+	private class Cube {
+		
+		public class Face {
+			private String TRCorner; // top right corner
+			private String TLCorner; // top left conrer
+			private String BRCorner; // bottom right corner
+			private String BLCorner; // bottom left corner
+		}
+		
+		public Face topFace;
+		public Face backFace;
+		public Face leftFace; 
+		public Face frontFace;
+		public Face rightFace;
+		public Face bottomFace;
+		
+	}
+	
+	public String solve(Cube c) {
+		return solve(cube, )
+	}
+	
+	public Cube backFaceRotateRight(Cube c){
+		
+	}
+	
+	public Cube backFaceRotateLeft(Cube c){
+		
+	}
+	
+	public Cube frontFaceRotateRight(Cube c){
+		
+	}
+	
+	public Cube frontFaceRotateLeft(Cube c){
+		
+	}
+	
+	public Cube leftFaceRotateBack(Cube c){
+		
+	}
+	
+	public Cube rightFaceRotateFront(Cube c){
+		
+	}
+	
+	public Cube rightFaceRotateBack(Cube c){
+		
+	}
+	
+	public Cube topFaceRotateRight(Cube c){
+		
+	}
 
+	public Cube topFaceRotateLeft(Cube c){
+		
+	}
+	
+	public Cube bottomFaceRotateRight(Cube c){
+		
+	}
+
+	public Cube bottomFaceRotateLeft(Cube c){
+		
+	}
+	
 }
