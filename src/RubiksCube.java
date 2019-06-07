f211442d62ab7bf6370e6a9cf5d05085a2d6325d
jeterarellano <jeterarellano@10.0.0.151>
2019-06-06 20:22:05 -0700

    Modified the Face and Cube classes such that they are arrays and easier to deal with. Began on the solve method as well as included Martin's getFace method that will possibly be used.

--- src/RubiksCube.java
+++ src/RubiksCube.java
@@ -4,23 +4,50 @@ public class RubiksCube {
 	private class Cube {
 		
 		public class Face {
-			private String TRCorner; // top right corner
-			private String TLCorner; // top left conrer
-			private String BRCorner; // bottom right corner
-			private String BLCorner; // bottom left corner
+			public final String[][] face = new String[2][2];
+			
+			public Face(String tr, String tl, String br, String bl) {
+				face[0][0] = tl;
+				face[0][1] = tr;
+				face[1][0] = bl;
+				face[1][1] = br;
+			}
 		}
 		
-		public Face topFace;
-		public Face backFace;
-		public Face leftFace; 
-		public Face frontFace;
-		public Face rightFace;
-		public Face bottomFace;
+		public final Face[] faces = new Face[6];
 		
+		public Cube (Face tf, Face bf, Face lf, Face ff, Face rf, Face bof) {
+			faces[0] = tf; // Top face is 1!
+			faces[1] = bf; // Back face is 2!
+			faces[2] = lf; // Left face is 3!
+			faces[3] = ff; // Front face is 4!
+			faces[4] = rf; // Right face is 5!
+			faces[5] = bof; // Bottom face is 6!
+		}
 	}
 	
+	// public class Move
+	
 	public String solve(Cube c) {
-		return solve(cube, )
+		return solve(c,  )
+	}
+	
+	public static int getFace(long cube, int n) {
+		
+		long div = 1;
+		for (int i = 0; i < n; i++) div *= 6;
+		cube /= div;
+		return (int)(cube % 6);
+	}
+	
+	public String solve(Cube c, int movesLeft) {
+		if (movesLeft == 0) {
+			for (RubiksCube.Cube.Face ): c.faces) {
+				for (String color: c.faces[faceID]) {
+					
+				}
+			}
+		}
 	}
 	
 	public Cube backFaceRotateRight(Cube c){
@@ -43,14 +70,6 @@ public class RubiksCube {
 		
 	}
 	
-	public Cube rightFaceRotateFront(Cube c){
-		
-	}
-	
-	public Cube rightFaceRotateBack(Cube c){
-		
-	}
-	
 	public Cube topFaceRotateRight(Cube c){
 		
 	}
@@ -58,13 +77,4 @@ public class RubiksCube {
 	public Cube topFaceRotateLeft(Cube c){
 		
 	}
-	
-	public Cube bottomFaceRotateRight(Cube c){
-		
-	}
-
-	public Cube bottomFaceRotateLeft(Cube c){
-		
-	}
-	
 }
